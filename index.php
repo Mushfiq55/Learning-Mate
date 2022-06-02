9fc84e4895bba1027253e0436121c6b8b2d1b027
Mushfiq55 <106772269+Mushfiq55@users.noreply.github.com>
2022-06-02 15:37:16 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>| Learning Mate |</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="shortcut icon" type="image/png" href="image/logo.png" />
+        <style type="text/css">
+            body {
+                width: 100%;
+                background: url(image/book.png) ;
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+            <div class="intro">
+                <h1> Learning Mate</h1>
+                <a href="login.php" class="btn"> Student Login </a>   
+                <a href="register.php" class="btn"> Register </a>   
+                <a href="admin.php" class="btn"> Admin Login </a>
+                <h2> An Absoulute E-Learning Platform </h2>
+            </div>
+
+        </center>
+    </body>
+</html>
